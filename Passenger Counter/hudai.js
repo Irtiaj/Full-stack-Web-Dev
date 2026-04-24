dbbbe4c56f20c9e8f1beb36ca7ac5143f0408c94
Irtiaj <irtiaj0541@gmail.com>
2026-04-24 19:20:18 +0600

    Delete Passenger Counter/hudai.js

--- Passenger Counter/hudai.js
+++
@@ -1,9 +0,0 @@
-
-let username = "Irtiaj"
-let greeting = "Assalamu Alaikum, Brother"
-
-let myGreeeting = greeting + " " +username
-console.log(myGreeeting)
-
-let welcomeEl = document.getElementById("welcome-el")
-welcomeEl.innerText = myGreeeting
